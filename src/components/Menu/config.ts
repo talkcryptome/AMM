7a9c0718e0788d4a48f9778e1857a88a75a8e313
talkcryptome <talkcryptowithme@gmail.com>
2022-03-04 23:58:55 -0700

    Initial Commit

---
+++ src/components/Menu/config.ts
@@ -0,0 +1,66 @@
+import { MenuEntry } from '@pancakeswap-libs/uikit'
+
+const config: MenuEntry[] = [
+  {
+    label: 'Home',
+    icon: 'HomeIcon',
+    href: 'https://talkcryptotome.com/',
+  },
+  {
+    label: 'Trade',
+    icon: 'TradeIcon',
+    items: [
+      {
+        label: 'Exchange',
+        href: 'https://exchange.talkcryptotome.com/#/exchange',
+      },
+      {
+        label: 'Liquidity',
+        href: 'https://exchange.talkcryptotome.com/#/liquidity',
+      },
+    ],
+  },
+  {
+    label: 'Farms',
+    icon: 'FarmIcon',
+    href: 'https://talkcryptotome.com/farm',
+  },
+  {
+    label: 'Pools',
+    icon: 'PoolIcon',
+    href: 'https://talkcryptotome.com/pool',
+  },
+  {
+    label: 'Vault',
+    icon: 'MoonIcon',
+    href: 'https://talkcryptotome.com/vault',
+  },
+  {
+    label: 'Bridge',
+    icon: 'TicketIcon',
+    href: 'https://talkcryptotome.com/bridge',
+  },
+  {
+    label: 'Aggregator',
+    icon: 'SunIcon',
+    href: 'https://talkcryptotome.com/tin',
+  },
+  {
+    label: 'Charts',
+    icon: 'HamburgerIcon',
+    items: [
+      {
+        label: 'DexGuru',
+        href: 'https://talkcryptotome.com/dex',
+      },
+      {
+        label: 'PooCoins',
+        href: 'https://talkcryptotome.com/poo',
+      },
+    ],
+  },
+
+ 
+]
+
+export default config
---
+++ src/constants/tacoPriceContracts.ts
@@ -0,0 +1,9 @@
+const priceContracts: { cakeAddress: string; busdAddress: string; lpAddress: string } = {
+  // cakeAddress: '0x9066e87Bac891409D690cfEfA41379b34af06391',
+  busdAddress: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
+  // lpAddress: '0x3BB3B98c0FABE30166E6B7D62aEef393249871E4',
+  cakeAddress: '0xBCFeDe73a76E2294B82d28c2737F9eECdf0342eB',
+  lpAddress: '0xc3bdff1fa4985b9977dddf2ea2b98c75dd4ec65c',
+}
+
+export default priceContracts
